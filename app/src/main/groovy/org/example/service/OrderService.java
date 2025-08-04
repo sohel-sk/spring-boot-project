eef3fb02b1c4549d0854aa1ac9e18e7d8111fc3d
sohel-sk <sohelsheikh9623@gmail.com>
2025-08-04 23:50:13 +0530

    IOC learning Dependency injector

---
+++ app/src/main/groovy/org/example/service/OrderService.java
@@ -0,0 +1,8 @@
+package org.example.service;
+import org.example.bean.UserConfig;
+public class OrderService {
+
+    public static OrderService createInstance(UserService userService,UserConfig userConfig) {
+        return null;
+    }
+    }
---
+++ app/src/main/groovy/org/example/service/UserService.java
@@ -0,0 +1,13 @@
+package org.example.service;
+
+import org.example.bean.UserConfig;
+
+public class UserService {
+
+    UserConfig userConfig ;
+
+
+    public UserService(UserConfig userConfig) {
+        this.userConfig = userConfig;
+    }
+}
